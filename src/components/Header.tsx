bf6651b995705bc4fb9e890496380d08d3e4b3a7
고태진 <thereisnotruth12@gmail.com>
2023-05-23 11:38:40 +0900

    mobile header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import { AiOutlineMenu } from '@react-icons/all-files/ai/AiOutlineMenu';
 
 function Header() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleNavBtnClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="header">
       <div className="header__content">
         <div className="header__logo hana">KOEH's Portfolio</div>
-        <li className="header__nav hana">
-          <AiOutlineMenu className="header__nav-btn" />
+        <button className="header__nav-btn" onClick={handleNavBtnClick}>
+          <AiOutlineMenu />
+        </button>
+      </div>
+      <div className={`header__nav ${isOpen ? 'header__nav--clicked' : ''}`}>
+        <li className="hana">
           <ul>Profile</ul>
           <ul>Skills</ul>
           <ul>Education</ul>
