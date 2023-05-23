1932a2bc0eb1505da20404f9f5925125199699e4
고태진 <thereisnotruth12@gmail.com>
2023-05-23 16:04:51 +0900

    mobile educations

--- src/components/Educations.tsx
+++ src/components/Educations.tsx
@@ -1,4 +1,30 @@
+import { BsPencilFill } from 'react-icons/bs';
+
+import educationJson from '@data/educations.json';
+
+interface Education {
+  name: string;
+  major: string;
+  period: string;
+}
+
 function Educations() {
-  return <div className="education">교육사항</div>;
+  return (
+    <div className="educations">
+      <div className="educations__container">
+        <div className="educations__title hana">Educations</div>
+        {educationJson['educations'].map((education: Education) => (
+          <div className="educations__row">
+            <BsPencilFill className="educations__icon" />
+            <div className="educations__label hana">
+              <div>{education.name}</div>
+              <div>{education.major}</div>
+            </div>
+            <div className="educations__content hana">{education.period}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 export default Educations;
