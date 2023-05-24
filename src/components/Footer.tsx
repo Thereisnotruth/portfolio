17dec62c3454ca0e852376ad18fbcd37f5cc1cf7
고태진 <thereisnotruth12@gmail.com>
2023-05-24 11:28:48 +0900

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,27 @@
+import { AiOutlineCopyright, AiOutlineGithub, AiFillLinkedin } from 'react-icons/ai';
+import { SiVelog } from 'react-icons/si';
+import profileJson from '@data/profile.json';
+
 function Footer() {
-  return <footer className="footer">푸터</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <a href={profileJson.git}>
+          <AiOutlineGithub />
+        </a>
+        <a href={profileJson.blog}>
+          <SiVelog />
+        </a>
+        <a href={profileJson.linkedIn}>
+          <AiFillLinkedin />
+        </a>
+        <div className="footer__copyright">
+          <AiOutlineCopyright className="footer__copyright-icon" />
+          2023. KOEH. all rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
