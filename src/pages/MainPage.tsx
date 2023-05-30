d5ae7ba4e1f14dc6021b88ea7c74618795588bb2
고태진 <thereisnotruth12@gmail.com>
2023-05-30 12:51:00 +0900

    add main page container dark mod

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useRecoilValue } from 'recoil';
 
 import Header from '@components/Header';
 import Footer from '@components/Footer';
@@ -6,12 +7,14 @@ import Profile from '@components/Profile';
 import Skills from '@components/Skills';
 import Educations from '@components/Educations';
 import Projects from '@components/Projects';
+import { modState } from '@atoms/modState';
 
 function MainPage() {
   // const [headerHeight, setHeaderHeight] = useState<number>(0);
   const [marginTop, setMarginTop] = useState<number>(0);
+  const theme = useRecoilValue(modState);
   return (
-    <div className="main-page">
+    <div className={`main-page ${theme}`}>
       <Header setMarginTop={setMarginTop} />
       <div className="main-page__content" style={{ marginTop: marginTop }}>
         <Profile />
